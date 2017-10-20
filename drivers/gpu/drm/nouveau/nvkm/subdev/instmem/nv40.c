e07f321d3e215ea28d266caf23a15531203ba343
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2017-10-20 16:22:44 +0100

    nouveau: avoid write-combine mappings for VRAM and IO space
    NOUPSTREAM
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Graeme Gregory <graeme.gregory@linaro.org>
    [daniel.thompson@linaro.org:
      Updated to undo changes to the bar2 mapping in v4.15-rc1, see
      dfcbd5506817 ("drm/nouveau/imem/nv50: map bar2 write-combined")
      and b807270cbd87 ("drm/nouveau/imem/nv40: map bar2
      write-combined") for more details]
    Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>

--- drivers/gpu/drm/nouveau/nvkm/subdev/instmem/nv40.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/instmem/nv40.c
@@ -253,8 +253,8 @@ nv40_instmem_new(struct nvkm_device *device, int index,
 	else
 		bar = 3;
 
-	imem->iomem = ioremap_wc(device->func->resource_addr(device, bar),
-				 device->func->resource_size(device, bar));
+	imem->iomem = ioremap(device->func->resource_addr(device, bar),
+			      device->func->resource_size(device, bar));
 	if (!imem->iomem) {
 		nvkm_error(&imem->base.subdev, "unable to map PRAMIN BAR\n");
 		return -EFAULT;
