dabf9b70533a620e7d7d9e9a0a3cb815a6a86c2b
Guido Günther <agx@sigxcpu.org>
2020-09-02 16:47:01 +0200

    leds: lm3692x: Drop probe deferrals
    With LCD_1V8 always on i2c3 becomes operational and
    we don't need to guess when that's the case.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>

--- drivers/leds/leds-lm3692x.c
+++ drivers/leds/leds-lm3692x.c
@@ -95,9 +95,6 @@
 #define LM3692X_FAULT_FLAG_SHRT BIT(3)
 #define LM3692X_FAULT_FLAG_OPEN BIT(4)
 
-#define MAX_DEFERS 2
-static int defers;
-
 /**
  * struct lm3692x_led -
  * @lock - Lock for reading/writing the device
@@ -482,11 +479,6 @@ static int lm3692x_probe(struct i2c_client *client,
 {
 	struct lm3692x_led *led;
 	int ret;
-
-	if (defers < MAX_DEFERS) {
-		defers++;
-		return -EPROBE_DEFER;
-	}
 
 	led = devm_kzalloc(&client->dev, sizeof(*led), GFP_KERNEL);
 	if (!led)
