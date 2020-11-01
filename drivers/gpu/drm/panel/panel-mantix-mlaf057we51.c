497f86a0468e21cbcfd03a776518194379bf23f6
Guido Günther <agx@sigxcpu.org>
2020-11-01 18:00:30 +0100

    drm/panel: mantix: Tweak init sequence
    We've seen some (non permanent) burn in and bad white balance
    on some of the panels. Adding this bit from a vendor supplied
    sequence fixes it.
    
    Closes: #226
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>

--- drivers/gpu/drm/panel/panel-mantix-mlaf057we51.c
+++ drivers/gpu/drm/panel/panel-mantix-mlaf057we51.c
@@ -21,6 +21,7 @@
 #define DRV_NAME "panel-mantix-mlaf057we51"
 
 /* Manufacturer specific Commands send via DSI */
+#define MANTIX_CMD_SPI_FINISH           0x90
 #define MANTIX_CMD_OTP_STOP_RELOAD_MIPI 0x41
 #define MANTIX_CMD_INT_CANCEL           0x4C
 
@@ -75,6 +76,10 @@ static int mantix_init_sequence(struct mantix *ctx)
 
 	dsi_generic_write_seq(dsi, MANTIX_CMD_OTP_STOP_RELOAD_MIPI, 0x5A, 0x09);
 	dsi_generic_write_seq(dsi, 0x80, 0x64, 0x00, 0x64, 0x00, 0x00);
+	msleep(20);
+
+	dsi_generic_write_seq(dsi, MANTIX_CMD_SPI_FINISH, 0xA5);
+	dsi_generic_write_seq(dsi, MANTIX_CMD_OTP_STOP_RELOAD_MIPI, 0x00, 0x2F);
 	msleep(20);
 
 	dev_dbg(dev, "Panel init sequence done\n");
